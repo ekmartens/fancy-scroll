074e1f59656ed771180c1e13d8aed3cd606dd444
ekmartens <ekmrtns@gmail.com>
2017-12-24 14:41:12 -0800

    initial elements

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 window.onscroll = function() {scrollFunction()};
 
-var block1H1 = document.getElementById('h1-test')
+var titlePage = document.getElementById('page1');
+var page2 = document.getElementById('page2');
+var interval1 = document.getElementById('interval1')
 
+//Page 2 Vars
+var bgVid1 = document.getElementById('bgvid1');
+var chapt1 = document.getElementById('chapt1');
+
+function entrance(element) {
+  element.style.display = "block";
+}
+
+function exit(element){
+  element.style.display = "none";
+}
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        block1H1.style.display = "block";
+    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+        exit(titlePage);
+        entrance(page2);
+        setTimeout(function() {
+          entrance(chapt1);
+        }, 1200);
+
     } else {
-        block1H1.style.display = "none"
+        entrance(titlePage);
+        exit(page2);
     }
 }
