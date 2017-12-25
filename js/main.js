964c13125ebdc5bc425fca95faf7eb056b0c0125
ekmartens <ekmrtns@gmail.com>
2017-12-24 23:45:04 -0800

    animate page 2

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ var cta1 = document.getElementById('ctaP1');
 
 //Page 2 Vars
 var page2 = document.getElementById('page2');
+var title2 = document.getElementById('pageTitle2');
+var list2 = document.getElementById('listP2');
+var point2 = document.getElementById('pointerP2');
+var nav2 = document.getElementById('nav-menu');
+var ourMenu = document.getElementById('menu-btn');
+var ourStory = document.getElementById('our-story-btn');
 
 //Page 3 Vars
 var page3 = document.getElementById('page3');
@@ -20,6 +26,52 @@ function exit(element){
   element.style.display = "none";
 }
 
+function closeP1(){
+  exit(titlePage);
+  exit(subTitle1);
+  exit(cta1);
+}
+
+function openP1(){
+  entrance(titlePage);
+  entrance(subTitle1);
+  subTitles(cta1,2500);
+}
+
+function closeP2(){
+  exit(page2);
+  exit(title2);
+  exit(list2);
+  exit(point2);
+  exit(nav2);
+}
+
+function openP2(){
+  entrance(page2);
+  setTimeout(function(){
+    entrance(title2);
+  }, 1000);
+  setTimeout(function(){
+  entrance(list2);
+  entrance(nav2);
+}, 3000);
+  setTimeout(function(){
+    entrance(point2);
+  }, 4000);
+}
+
+function closeP3() {
+  exit(page3);
+  exit(chapt1);
+}
+
+function openP3(){
+  entrance(page3);
+  setTimeout(function() {
+    entrance(chapt1);
+  }, 1200);
+}
+
 function subTitles(sub, interval){
   setTimeout(function(){
     sub.style.display = "block";
@@ -29,57 +81,30 @@ function subTitles(sub, interval){
 subTitles(subTitle1, 1000);
 subTitles(cta1, 2000);
 
+function navigateStory(){
+  closeP2();
+  openP3();
+}
+
+ourStory.addEventListener("click", navigateStory);
+point2.addEventListener("click", navigateStory);
+
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 300) {
-      exit(page2);
-      entrance(page3);
-      setTimeout(function() {
-        entrance(chapt1);
-      }, 1200);
+      closeP2();
+      openP3();
     }
     else if (scroll >= 150) {
-      exit(titlePage);
-      exit(subTitle1);
-      exit(cta1);
-      exit(page3);
-      entrance(page2);
+      closeP1();
+      closeP3();
+      openP2();
     }
     else {
-        exit(page2);
-        entrance(titlePage);
-        entrance(subTitle1);
-        subTitles(cta1,2500);
+        closeP2();
+        openP1();
     }
 });
 
 });
-
-/**
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        exit(titlePage);
-        exit(subTitle1);
-        exit(cta1);
-        exit(page3);
-        entrance(page2);
-    }
-
-    else if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      exit(page2);
-      entrance(page3);
-      setTimeout(function() {
-        entrance(chapt1);
-      }, 1200);
-
-    }
-
-    else {
-        exit(page2);
-        entrance(titlePage);
-        entrance(subTitle1);
-        subTitles(cta1,2500);
-    }
-}
-**/
