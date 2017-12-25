e3a48427f2f4f8f8aeef64b594565a8e73e90b3f
ekmartens <ekmrtns@gmail.com>
2017-12-24 19:41:33 -0800

    finish page 1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 var titlePage = document.getElementById('page1');
 var subTitle1 = document.getElementById('p1Subtitle');
+var cta1 = document.getElementById('ctaP1');
 
 //Page 2 Vars
 var page2 = document.getElementById('page2');
@@ -18,27 +19,31 @@ function exit(element){
   element.style.display = "none";
 }
 
-function subTitles(sub){
+function subTitles(sub, interval){
   setTimeout(function(){
-    sub.className = "fadeIn";
     sub.style.display = "block";
-  }, 2000);
+  }, interval);
 }
 
-subTitles(subTitle1);
+subTitles(subTitle1, 1000);
+subTitles(cta1, 2000);
+
 
 function scrollFunction() {
     if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
         exit(titlePage);
         exit(subTitle1);
+        exit(cta1)
         entrance(page2);
         setTimeout(function() {
           entrance(chapt1);
         }, 1200);
 
+
     } else {
-        entrance(titlePage);
         exit(page2);
-        subTitles(subTitle1);
+        entrance(titlePage);
+        entrance(subTitle1);
+        subTitles(cta1,2500);
     }
 }
